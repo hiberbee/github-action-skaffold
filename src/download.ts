b95598d326c54585808bf24a697899749ac49404
Vlad Volkov <vlad@hiberbee.com>
2020-08-02 01:40:14 +0300

    Fixed cache source

--- src/download.ts
+++ src/download.ts
@@ -1,7 +1,7 @@
 import { addPath } from '@actions/core'
 import { exec } from '@actions/exec'
 import { downloadTool } from '@actions/tool-cache'
-import { mkdirP, mv } from '@actions/io'
+import { mkdirP, mv, cp } from '@actions/io'
 import path from 'path'
 
 /**
@@ -15,14 +15,12 @@ import path from 'path'
  */
 export default async function (url: string, destination: string): Promise<string> {
   const downloadPath = await downloadTool(url)
-
   const destinationDir = path.dirname(destination)
   await mkdirP(destinationDir)
   if (url.endsWith('tar.gz') || url.endsWith('tar') || url.endsWith('tgz')) {
     await exec('tar', ['-xz', `--file=${downloadPath}`, `--directory=${destinationDir}`, `--strip=1`])
-  } else {
-    await mv(downloadPath, destination)
   }
+  await cp(downloadPath, destination)
   await exec('chmod', ['+x', destination])
   addPath(destinationDir)
   return downloadPath
